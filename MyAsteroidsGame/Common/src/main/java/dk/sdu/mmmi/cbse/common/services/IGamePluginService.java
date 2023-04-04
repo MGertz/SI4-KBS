2a7ba8faa46eb21811d3dae65fc217218fb96662
Michael Gertz <michael@ringhus.dk>
2023-04-04 11:18:39 +0200

    Added descriptions

--- MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -4,7 +4,26 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IGamePluginService {
+
+    /**
+     * This interface is responsible for loading plugins when the game is started.
+     *
+     * Pre-condition:  Starts the game, there is no plugins loaded.
+     * Post-condition: The plugins has been loaded.
+     *
+     * @param GameData gameData
+     * @param World world
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * This interface is responsible for unloaded plugins when the game stops.
+     *
+     * Pre-condition:  The game is started, and needs to stop, all plugins is loaded
+     * Post-condition: All plugins has been unloaded.
+     *
+     * @param GameData gameData
+     * @param World world
+     */
     void stop(GameData gameData, World world);
 }
--- MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -3,10 +3,14 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
-/**
- *
- * @author jcs
- */
 public interface IPostEntityProcessingService  {
-        void process(GameData gameData, World world);
+
+    /**
+     * Pre-condition: The game has looped once since last call. All entities has been processed.
+     * Post-condition: Entity is processed and updated.
+     *
+     * @param GameData gameData
+     * @param World world
+     */
+    void process(GameData gameData, World world);
 }
