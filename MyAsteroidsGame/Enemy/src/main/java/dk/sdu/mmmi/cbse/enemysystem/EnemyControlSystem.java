5636fc32342635fdd5b77f22d39f21f5778ada4f
Michael Gertz <michael@ringhus.dk>
2023-04-04 19:23:54 +0200

    Cleanup

--- MyAsteroidsGame/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
+++ MyAsteroidsGame/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -25,15 +25,15 @@ public class EnemyControlSystem implements IEntityProcessingService {
                 this.tickCounter = 0;
 
                 if (Math.random() < 0.5) {
-                    direction=0;
+                    direction = 0;
                 } else {
-                    direction=1;
+                    direction = 1;
                 }
             }
 
 
-            if( Math.random() < 0.2 ) {
-                if (direction==1) {
+            if (Math.random() < 0.2) {
+                if (direction == 1) {
                     movingPart.setRight(true);
                 } else {
                     movingPart.setLeft(true);
@@ -58,31 +58,6 @@ public class EnemyControlSystem implements IEntityProcessingService {
     }
 
     private void updateShape(Entity entity) {
-        /*
-        float[] shapex = entity.getShapeX();
-        float[] shapey = entity.getShapeY();
-        PositionPart positionPart = entity.getPart(PositionPart.class);
-        float x = positionPart.getX();
-        float y = positionPart.getY();
-        float radians = positionPart.getRadians();
-
-        shapex[0] = (float) (x + Math.cos(radians) * 8);
-        shapey[0] = (float) (y + Math.sin(radians) * 8);
-
-        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
-        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
-
-        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
-        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
-
-        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
-        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
-
-        entity.setShapeX(shapex);
-        entity.setShapeY(shapey);
-
-         */
-
         // This is how to get the original amount of x,y points.
         //float[] shapex = entity.getShapeX();
         //float[] shapey = entity.getShapeY();
