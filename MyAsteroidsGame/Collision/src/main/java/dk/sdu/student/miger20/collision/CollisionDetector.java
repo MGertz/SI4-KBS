8a2df7c3df41758a273fe6863a63655e24c12b81
Michael Gertz <michael@ringhus.dk>
2023-04-07 23:22:02 +0200

    Implemented correct collision detection

--- MyAsteroidsGame/Collision/src/main/java/dk/sdu/student/miger20/collision/CollisionDetector.java
+++ MyAsteroidsGame/Collision/src/main/java/dk/sdu/student/miger20/collision/CollisionDetector.java
@@ -1,11 +1,15 @@
 package dk.sdu.student.miger20.collision;
 
+import dk.sdu.student.miger20.asteroidsystem.Asteroid;
+import dk.sdu.student.miger20.bullet.Bullet;
 import dk.sdu.student.miger20.common.data.Entity;
 import dk.sdu.student.miger20.common.data.GameData;
 import dk.sdu.student.miger20.common.data.World;
 import dk.sdu.student.miger20.common.data.entityparts.LifePart;
 import dk.sdu.student.miger20.common.data.entityparts.PositionPart;
 import dk.sdu.student.miger20.common.services.IPostEntityProcessingService;
+import dk.sdu.student.miger20.enemysystem.Enemy;
+import dk.sdu.student.miger20.playersystem.Player;
 
 public class CollisionDetector implements IPostEntityProcessingService {
 
@@ -24,7 +28,41 @@ public class CollisionDetector implements IPostEntityProcessingService {
                 LifePart entity1Life = entity1.getPart(LifePart.class);
                 LifePart entity2Life = entity2.getPart(LifePart.class);
 
-                if (this.collides(entity1, entity2)) {
+
+                // Player collide with Asteroid
+                if ((entity1.getClass() == Player.class && entity2.getClass() == Asteroid.class) && this.collides(entity1, entity2)) {
+                    //System.out.println("Collision: 1");
+                    entity1Life.setIsHit(true);
+                }
+
+                // Enemy collide with Asteroid
+                if ((entity1.getClass() == Enemy.class && entity2.getClass() == Asteroid.class) && this.collides(entity1, entity2)) {
+                    //System.out.println("Collision: 2");
+                    entity1Life.setIsHit(true);
+                }
+
+                // Asteroid shot at
+                if ((entity1.getClass() == Asteroid.class && entity2.getClass() == Bullet.class) && this.collides(entity1, entity2)) {
+                    //System.out.println("Collision: 3");
+                    entity1Life.setIsHit(true);
+                }
+
+                // Player and Enemy Collide
+                if ((entity1.getClass() == Player.class && entity2.getClass() == Enemy.class) && this.collides(entity1, entity2)) {
+                    //System.out.println("Collision: 4");
+                    entity1Life.setIsHit(true);
+                    entity2Life.setIsHit(true);
+                }
+
+                // Player shot by bullet
+                if ((entity1.getClass() == Player.class && entity2.getClass() == Bullet.class) && this.collides(entity1, entity2)) {
+                    //System.out.println("Collision: 5");
+                    entity1Life.setIsHit(true);
+                }
+
+                // Enemy shot by bullet
+                if ((entity1.getClass() == Enemy.class && entity2.getClass() == Bullet.class) && this.collides(entity1, entity2)) {
+                    //System.out.println("Collision: 6");
                     entity1Life.setIsHit(true);
                 }
             }
@@ -39,7 +77,9 @@ public class CollisionDetector implements IPostEntityProcessingService {
         float dy = pP1.getY() - pP2.getY();
         float distance = (float) Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < (entity1.getRadius() + entity2.getRadius())) {
+        float totalRadius = entity1.getRadius() + entity2.getRadius();
+
+        if (distance < totalRadius) {
             return true;
         }
         return false;
