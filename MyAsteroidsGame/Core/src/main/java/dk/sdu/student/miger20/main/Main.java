3df83e2ef1d31493ad0ccf2a67a96c4d79b2c953
Michael Gertz <michael@ringhus.dk>
2023-05-07 12:17:10 +0200

    Updated libgdx

--- MyAsteroidsGame/Core/src/main/java/dk/sdu/student/miger20/main/Main.java
+++ MyAsteroidsGame/Core/src/main/java/dk/sdu/student/miger20/main/Main.java
@@ -1,21 +1,18 @@
 package dk.sdu.student.miger20.main;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "MyAsteroidsGame";
-		cfg.width = (1920/2);
-		cfg.height = (1080/2);
-		cfg.useGL30 = false;
-		cfg.resizable = true;
 
-		new LwjglApplication(new Game(), cfg);
-		
+		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+		cfg.setTitle("MyAsteroidsGame");
+		cfg.setWindowedMode(1920/2, 1080/2);
+		cfg.setResizable(false);
+
+		new Lwjgl3Application(new Game(), cfg);
 	}
 	
 }
